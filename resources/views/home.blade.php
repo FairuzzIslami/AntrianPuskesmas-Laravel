cb5b5d1f3fe17f34d29323bb2433cb0e1b97fc75
arhamlb17 <arhamlamabelawa3@gmail.com>
2025-09-18 08:15:00 +0700

    Merge remote-tracking branch 'origin/main'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 @extends('layout.layout')
 
-@section('title', 'Home Puskesmas')
-
 
 @section('content')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
